b810e4a811292e32f1450cf995697625e34d3fe2
Jack Robertson <jcrobertson97@gmail.com>
2020-11-21 11:57:41 +0000

    Add migration

--- migrations/Version20201120210730.php
+++ migrations/Version20201120210730.php
@@ -11,16 +11,25 @@ final class Version20201120210730 extends AbstractMigration
 {
     public function getDescription() : string
     {
-        return ''; // Todo
+        return 'Creates the images table.';
     }
 
     public function up(Schema $schema) : void
     {
-        // Todo
+        $this->addSql('
+            CREATE TABLE images (
+                id INT(11) AUTO_INCREMENT PRIMARY KEY,
+                file_name VARCHAR(255) UNIQUE NOT NULL,
+                width INT(11) NOT NULL,
+                height INT(11) NOT NULL,
+                focal_point_x INT(11) NOT NULL,
+                focal_point_y INT(11) NOT NULL
+            );
+        ');
     }
 
     public function down(Schema $schema) : void
     {
-        // Todo
+        $this->addSql('DROP TABLE IF EXISTS images');
     }
 }
